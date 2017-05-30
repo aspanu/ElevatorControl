d3b590f0af0da48366ab45c0b637748b6b02c141
Adrian Spanu <adri@nspanu.ca>
2017-05-30 22:06:45 +0200

    First commit creating the maven project.

---
+++ src/main/kotlin/com/spanu/elevator_control/Hello.kt
@@ -0,0 +1,6 @@
+package com.spanu.elevator_control
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/spanu/elevator_control/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.spanu.elevator_control
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
