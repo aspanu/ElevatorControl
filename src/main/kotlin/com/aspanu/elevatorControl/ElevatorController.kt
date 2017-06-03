5557cc8499ece5e799c4ee42edb9f5a6637273fc
Adrian Spanu <adri@nspanu.ca>
2017-06-03 11:07:28 +0200

    Adding Spek to the project and a Readme.

---
+++ src/main/kotlin/com/aspanu/elevatorControl/ElevatorController.kt
@@ -0,0 +1,6 @@
+package com.aspanu.elevatorControl
+
+/**
+ * Created by aspanu on 2017-06-03
+ */
+
---
+++ src/test/kotlin/com/aspanu/elevatorControl/ElevatorControllerSpec.kt
@@ -0,0 +1,12 @@
+package com.aspanu.elevatorControl
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.Spec
+
+/**
+ * Created by aspanu on 2017-06-03.
+ */
+
+class ElevatorControllerSpec(spec: Spec.() -> Unit) : Spek(spec) {
+
+}
