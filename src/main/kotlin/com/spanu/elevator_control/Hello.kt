23410aaa35f47db6c81df15780b46516f1015632
Adrian Spanu <adri@nspanu.ca>
2017-06-03 10:54:23 +0200

    Migrating the project to Gradle.

--- src/main/kotlin/com/spanu/elevator_control/Hello.kt
+++
@@ -1,6 +0,0 @@
-package com.spanu.elevator_control
-
-fun main(args: Array<String>) {
-    println("Hello, World")
-}
-
--- src/test/kotlin/com/spanu/elevator_control/HelloTest.kt
+++
@@ -1,8 +0,0 @@
-package com.spanu.elevator_control
-
-import org.junit.Test
-import kotlin.test.assertEquals
-
-class HelloTest {
-
-}
